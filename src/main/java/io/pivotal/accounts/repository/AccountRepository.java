1bb79a54c1408bf17f87cff2d9476603878a4f13
Rajan <rajan@Rajans-MBP.attlocal.net>
2022-10-31 14:46:11 -0500

    Initial Commit

---
+++ src/main/java/io/pivotal/accounts/repository/AccountRepository.java
@@ -0,0 +1,18 @@
+package io.pivotal.accounts.repository;
+
+import io.pivotal.accounts.domain.Account;
+import io.pivotal.accounts.domain.AccountType;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AccountRepository extends CrudRepository<Account,Integer> {
+
+	@Query("from Account where type = :type")
+	List<Account> findByUseridAndType(@Param("type") AccountType type);
+
+	@Query("from Account")
+    List<Account> findByUserid();
+}
